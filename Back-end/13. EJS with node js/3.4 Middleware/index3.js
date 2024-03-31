d039fcd09e57d576ce9b1f870cff1273b768a522
ADITYA NEVE <64825422+Aditya21neve@users.noreply.github.com>
2024-03-31 12:46:57 +0530

    custom middleware

--- Back-end/13. EJS with node js/3.4 Middleware/index3.js
+++ Back-end/13. EJS with node js/3.4 Middleware/index3.js
@@ -1,7 +1,7 @@
 import express from "express";
-
 const app = express();
 const port = 3000;
+
 
 app.use(logger);
 
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+function logger(req , res, next){
+  console.log("This is req:  ",req.method);
+  console.log("This is url: " , req.url);
+  next();
+} 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+ 
